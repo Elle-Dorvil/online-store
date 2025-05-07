70fdd4d2a8fe16a770b5261720eb31b2d9cb709e
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-07 00:37:03 -0400

    Struugling to get the load inventory methos to work and display cart

--- src/main/java/com/pluralsight/Store.java
+++ src/main/java/com/pluralsight/Store.java
@@ -39,9 +39,9 @@ public class Store {
                 case 1:
                     displayProducts(inventory, cart, scanner);
                     break;
-                case 2:
-                    displayCart(cart, scanner, totalAmount);
-                    break;
+//                case 2:
+//                    displayCart(cart, scanner, totalAmount);
+//                    break;
                 case 3:
                     System.out.println("Thank you for shopping with us!");
                     break;
@@ -69,19 +69,12 @@ public class Store {
                 String productId = parts[0];
                 String productName = parts[1];
                 double price = Double.parseDouble(parts[2]);
-
                 inventory.add(new Product(productId, productName, price));
-
             }
             br.close();
-
-
         } catch (Exception e) {
-            System.out.println("Invalid input. ");
-
+            System.out.print("Invalid input. ");
         }
-
-
     }
 
     public static void displayProducts(ArrayList<Product> inventory, ArrayList<Product> cart, Scanner scanner) {
@@ -90,43 +83,52 @@ public class Store {
         // prompt the user to enter the ID of the product they want to add to
         // their cart. The method should
         // add the selected product to the cart ArrayList.
+        String productId = scanner.nextLine();
         System.out.println("Please add your item to the cart: ");
         scanner.nextLine();
         System.out.println("Please enter the Product Id: ");
+        scanner.nextLine();
         scanner.nextLine();
 
         for (Product product : inventory) {
             System.out.println(product);
             System.out.printf(" %-30s %-20s %-10s%n", "Product Id", "Product Name", "Price");
 
-            if (product.getProductId().equals(product) ) {
+            if (product.getProductId() == productId) {
                 System.out.println(product);
-                
+
             }
+            return;
 
         }
     }
 
-    public static void displayCart(ArrayList<Product> cart, Scanner scanner, double totalAmount) {
-        // This method should display the items in the cart ArrayList, along
-        // with the total cost of all items in the cart. The method should
-        // prompt the user to remove items from their cart by entering the ID
-        // of the product they want to remove. The method should update the cart ArrayList and totalAmount
-        // variable accordingly.
-    }
-
-    public static void checkOut(ArrayList<Product> cart, double totalAmount) {
-        // This method should calculate the total cost of all items in the cart,
-        // and display a summary of the purchase to the user. The method should
-        // prompt the user to confirm the purchase, and calculate change and clear the cart
-        // if they confirm.
-    }
-
-    public static Product findProductById(String id, ArrayList<Product> inventory) {
-        // This method should search the inventory ArrayList for a product with
-        // the specified ID, and return the corresponding com.pluralsight.Product object. If
-        // no product with the specified ID is found, the method should return
-        // null.
-    }
+//    public static void displayCart(ArrayList<Product> cart, Scanner scanner, double totalAmount) {
+//        // This method should display the items in the cart ArrayList, along
+//        // with the total cost of all items in the cart. The method should
+//        // prompt the user to remove items from their cart by entering the ID
+//        // of the product they want to remove. The method should update the cart ArrayList and totalAmount
+//        // variable accordingly.
+//        String
+//        for (Product product : cart) {
+//            System.out.println(product);
+//            System.out.printf(" %-30s %-20s %-10s%n", "Product Id", "Product Name", "Price");
+//
+//        }
+//    }
+//
+//    public static void checkOut(ArrayList<Product> cart, double totalAmount) {
+//        // This method should calculate the total cost of all items in the cart,
+//        // and display a summary of the purchase to the user. The method should
+//        // prompt the user to confirm the purchase, and calculate change and clear the cart
+//        // if they confirm.
+//    }
+//
+//    public static Product findProductById(String id, ArrayList<Product> inventory) {
+//        // This method should search the inventory ArrayList for a product with
+//        // the specified ID, and return the corresponding com.pluralsight.Product object. If
+//        // no product with the specified ID is found, the method should return
+//        // null.
+//    }
 }
 
